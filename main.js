de5b3e09713346b31d6a6d14667cd2cc28a81161
Pawel Jakimiuk <jakim87@gmail.com>
2019-07-05 23:00:27 +0200

    css change

---
+++ main.js
@@ -0,0 +1,9 @@
+//variables
+const bDay = '1987-05-09'
+const name = 'Paweł'
+const surname = 'Jakimiuk'
+
+console.log('To jest alert. Co by tu zrobic??')
+console.log(name)
+
+console.log(`Nazywam sie ${name} ${surname}. Ucze sie HTMLa.`)
